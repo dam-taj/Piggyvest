8c16b440a2d83596225f059d9b55e35ea9aa5799
Nancy Damilola Taju <71294877+dam-taj@users.noreply.github.com>
2022-12-04 11:04:23 +0100

    In progress

---
+++ script.js
@@ -0,0 +1,12 @@
+document.documentElement.style.setProperty("--animate-duration", ".5s");
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
